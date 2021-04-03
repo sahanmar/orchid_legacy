0d2cc0e5dde4bb5b951477ec5d4176d117a650dc
Marko Sahan <marko.sahan@gmail.com>
2021-04-03 16:58:33 +0200

    parsed data from doc to ConllSentence Data structure

--- data_processing/conll_parses.py
+++ data_processing/conll_parses.py
@@ -2,12 +2,14 @@ import argparse
 import sys
 
 from pathlib import Path
-from typing import Tuple, Optional
+from typing import Tuple, Optional, List, Dict
+from utils.util_types import ConllSentence, TokenRange, Morphology, CorrefTokenType
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", help="Path to conll input file")
+    parser.add_argument("-t", "--type", choices=["train", "test", "validate"])
     return parser.parse_args()
 
 
@@ -17,9 +19,92 @@ def path_check(path: Path) -> Optional[Path]:
     return None
 
 
-if __name__ == "__main__":
+def split_file_2_texts(path: Path) -> List[List[str]]:
+
+    texts: List[List[str]] = []
+    sentence: List[str] = []
+
+    with open(path, "r") as f:
+        for line in f.readlines():
+            if line.startswith("#begin document") or line.startswith("#end document"):
+                continue
+            if not line.strip():
+                texts.append(sentence)
+                del sentence
+                sentence = []
+                continue
+            sentence.append(line)
+    return texts
+
+
+def parse_cr(text: str) -> Tuple[int, CorrefTokenType]:
+    if text.startswith("("):
+        if text.endswith(")"):
+            return int(text[1:-1]), CorrefTokenType.full
+        return int(text[1:]), CorrefTokenType.start
+    return int(text[:-1]), CorrefTokenType.end
+
+
+def add_corref_mutable(corref_dict: Dict[int, TokenRange], unprocessed_corref: str, i_token: int) -> None:
+    for corref in unprocessed_corref.split("|"):
+        cr_label, cr_type = parse_cr(corref)
+        if cr_label not in corref_dict:
+            corref_dict[cr_label] = []
+        if cr_type == CorrefTokenType.full:
+            corref_dict[cr_label].append(TokenRange(start=i_token, end=i_token + 1))
+        elif cr_type == CorrefTokenType.start:
+            corref_dict[cr_label].append(TokenRange(start=i_token, end=i_token))
+        else:
+            corref_dict[cr_label][-1].end = i_token + 1
+
+
+def process_sentences(sentence: List[str]) -> ConllSentence:
+
+    tokens: Dict[int, Morphology] = {}
+    correferences: Dict[int, TokenRange] = {}
+
+    for i, per_token_annotations in enumerate(sentence):
+        (
+            folder,
+            i_sent_str,
+            i_token_str,
+            token,
+            pos,
+            _,
+            lemma,
+            _,
+            _,
+            speaker,
+            *rest,
+        ) = per_token_annotations.split()
+        i_token = int(i_token_str)
+
+        cr = rest[-1]
+        tokens[i_token] = Morphology(text=token, part_of_speech=pos, lemma=lemma if lemma != "-" else token)
+
+        if not cr.startswith("-"):
+            add_corref_mutable(correferences, cr, i_token)
+
+    return ConllSentence(
+        folder=folder,
+        sentence_index=int(i_sent_str),
+        word_tokens=tokens,
+        speaker=speaker,
+        correferences=correferences,
+    )
+
+
+def main():
     args = parse_args()
     path = path_check(Path(args.input))
+
     if not path:
         print(f"The path '{args.input}' is not a file")
         sys.exit()
+
+    texts = split_file_2_texts(path)
+    processed_sentences = [process_sentences(sentence) for sentence in texts]
+
+
+if __name__ == "__main__":
+    main()
--- utils/util_types.py
+++ utils/util_types.py
@@ -1,17 +1,44 @@
 from dataclasses import dataclass
-from typing import Optional, Any, Callable
+from enum import Enum
+
+from typing import Optional, Any, Callable, Dict, List
+
+
+class CorrefTokenType(Enum):
+    """
+    Correferences are labeled as spans. Thus, more than one 
+    consecutive words can be assigned to the same correference. 
+    We distinguish 
+    start <- the begging of the span
+    end <- the end of the span
+    full <- the correference is one token
+    """
+
+    start = 1
+    end = 2
+    full = 3
+
+
+class TokenRange:
+    def __init__(self, start: int, end: int):
+        self.start = start
+        self.end = end
+
+    def __repr__(self):
+        return f"({self.start},{self.end})"
 
 
 @dataclass
-class ConllDataType:
+class Morphology:
     text: str
-    word_tokens: str
-    speaker: str
-    coreferences:
+    part_of_speech: str
+    lemma: str
 
 
 @dataclass
-class Corefence:
-    label: int,
-    coref_range: TokenRange
-
+class ConllSentence:
+    folder: str
+    sentence_index: int
+    word_tokens: Dict[int, Morphology]
+    speaker: str
+    correferences: Dict[int, TokenRange]
